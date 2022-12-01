3003bca80fb280effed1a34cc1ca7c44295ee958
Sunil Chauhan <Sunil Chauhan@Sunil-Chauhan>
2022-12-01 17:04:29 +0530

    First commit

---
+++ JavaProject/src/demo/democlass.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class democlass {
+	public static void main(String[] args) {
+		System.out.println(" i m in demo class");
+		System.out.println(" i m in demo class");
+
+		System.out.println(" i m in demo class");
+
+		System.out.println(" i m in demo class");
+
+		System.out.println(" i m in demo class");
+
+	}
+}
